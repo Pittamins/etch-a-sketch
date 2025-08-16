82ddf0d89a710caa0e41c15597cfa0043d2baf0b
Pittamins <216646151+Pittamins@users.noreply.github.com>
2025-08-15 20:35:57 -0700

    Add button for custom grid sizing
    Still need to figure out how to limit custom
    dimensions to numbers between 1-100 only.

--- javascript.js
+++ javascript.js
@@ -13,11 +13,32 @@ function createGrid(num) {
     };
 };
 
-createGrid(16);
+const column = document.querySelector(".column");
+
+let dimension = 16;
+
+createGrid(dimension);
 
 const box = document.querySelector(".box");
 
 container.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = "salmon";
-    });
+});
 
+const custom = document.querySelector("#custom");
+
+custom.addEventListener("click", (event) => {
+    removeGrid(dimension);
+    dimension = prompt("Grid Dimension:", "");
+    if (dimension > 100 || dimension < 1) {
+        alert("Please select a dimension between 1 - 100")
+        dimension = prompt("Grid Dimension:", "");
+    };
+    createGrid(dimension);
+});
+
+function removeGrid(dimension) {
+    for (i = 0; i < dimension; i++) {
+        container.removeChild(container.lastChild);
+    };
+};
